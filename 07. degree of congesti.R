c7a9a41338a391370f2debca500b29bcd7f0aa6e
seoyunpark <pse-021107@naver.com>
2023-10-29 03:46:46 +0900

    07.degree of congesti

--- 07. degree of congesti.R
+++ 07. degree of congesti.R
@@ -2,7 +2,7 @@
 library(dplyr)
 library(ggplot2)
 
-# csv형식의 파일 불러와서 subway 객체에 입력하고 구조 확인
+# csv형식의 파일 불러와서 congestion 객체에 입력하고 구조 확인
 str(congestion)
 
 # 결측치 데이터 확인하기
@@ -64,55 +64,51 @@ s0900_mean <- congestion1 %>%
 total_mean <- (s0700_mean + s0730_mean + s0800_mean + s0830_mean + s0900_mean) / 5
 print(total_mean)
 
-#01. 기술통계분석
-summary(congestion1$s0700)
-summary(congestion1$s0730)
-summary(congestion1$s0800)
-summary(congestion1$s0830)
-summary(congestion1$s0900)
+  #01. 기술통계분석
+  summary(congestion1$s0700)
+  summary(congestion1$s0730)
+  summary(congestion1$s0800)
+  summary(congestion1$s0830)
+  summary(congestion1$s0900)
 
-line_average = c(congestion1$s0700, congestion1$s0730, congestion1$s0800, congestion1$s0830, congestion1$s0900)
-summary(line_average, na_rm=True)
+  line_average = c(congestion1$s0700, congestion1$s0730, congestion1$s0800, congestion1$s0830, congestion1$s0900)
+  summary(line_average, na_rm=True)
 
-#02. 평균 혼잡도가 가장 높은 시간대의 막대 그래프
+  #02. 평균 혼잡도가 가장 높은 시간대의 막대 그래프
 
-congestion1 %>%
-  summarise(s0700_mean = mean (s0700, na.rm = TRUE))
+  congestion1 %>%
+    summarise(s0700_mean = mean (s0700, na.rm = TRUE))
 
-congestion1 %>%
-  summarise(s0730_mean = mean (s0730, na.rm = TRUE))
+  congestion1 %>%
+    summarise(s0730_mean = mean (s0730, na.rm = TRUE))
 
-congestion1 %>%
-  summarise(s0800_mean = mean (s0800, na.rm = TRUE))
+  congestion1 %>% 
+    summarise(s0800_mean = mean (s0800, na.rm = TRUE))
 
-congestion1 %>%
-  summarise(s0830_mean = mean (s0830, na.rm = TRUE))
+  congestion1 %>%
+    summarise(s0830_mean = mean (s0830, na.rm = TRUE))
 
-congestion1 %>%
-  summarise(s0900_mean = mean (s0900, na.rm = TRUE))
+  congestion1 %>%
+    summarise(s0900_mean = mean (s0900, na.rm = TRUE))
 
-v = c(18.7, 24.6,32.3,30.1,29.4)
-barplot(v)
-name = c('s0700','s0730','s0800','s0830','s0900')
-barplot(v,names=name) # barplot(v~name)
+  v = c(18.7, 24.6,32.3,30.1,29.4)
+  barplot(v)
+  name = c('s0700','s0730','s0800','s0830','s0900')
+  barplot(v,names=name) # barplot(v~name)
 
-#03. 평균 혼잡도 상위 4개 호선의 역별 기여도
+  #03. 평균 혼잡도 상위 4개 호선의 역별 기여도
  
+  # 추가 열을 비교하기 위해 필요한 열 이름을 나열
+  columns_to_compare <- c("day_mean", "s0700", "s0730", "s0800", "s0830", "s0900")
 
-# 추가 열을 비교하기 위해 필요한 열 이름을 나열
-library(dplyr)
+  # "congestion" 데이터 프레임을 이용하여 "line" 별로 열들을 비교
+  result <- congestion1 %>%
+    group_by(line, station) %>%
+    summarize(across(all_of(columns_to_compare), mean)) %>%
+    arrange(desc(day_mean)) %>%
+    head(4)
 
-# 추가 열을 비교하기 위해 필요한 열 이름을 나열
-columns_to_compare <- c("day_mean", "s0700", "s0730", "s0800", "s0830", "s0900")
-
-# "congestion" 데이터 프레임을 이용하여 "line" 별로 열들을 비교
-result <- congestion1 %>%
-  group_by(line, station) %>%
-  summarize(across(all_of(columns_to_compare), mean)) %>%
-  arrange(desc(day_mean)) %>%
-  head(4)
-
-print(result)
+  print(result)
 
 
 #4. 출발시간 08시 지하철 혼잡도 범주화/범주별 빈도분석
@@ -144,11 +140,11 @@ total_mean1 <- (s1800_mean$s1800_mean + s1830_mean$s1830_mean + s1900_mean$s1900
 print(total_mean1)
 
    #01. 기술통계분석 결과
-summary(congestion1$s1800)
-summary(congestion1$s1830)
-summary(congestion1$s1900)
-summary(congestion1$s1930)
-summary(congestion1$s2000)
+  summary(congestion1$s1800)
+  summary(congestion1$s1830)
+  summary(congestion1$s1900)
+  summary(congestion1$s1930)
+  summary(congestion1$s2000)
 
   #02. 가장 높은 시간대의 막대그래프 확인하기기
 
@@ -173,8 +169,7 @@ summary(congestion1$s2000)
   barplot(v,names=name) # barplot(v~name) 
   
    #03. 평균혼잡동 상위 4개 호선의 역별 기여도
-  library(dplyr)
-  
+
   # 추가 열을 비교하기 위해 필요한 열 이름을 나열
   columns_to_compare <- c("day_mean", "s1800", "s1830", "s1900", "s1930", "s2000")
   
